8e417c19b4c07669eec3eab42815e180d5cfc62b
AlysonEast <a.east.ecology@gmail.com>
2023-05-31 09:18:41 -0400

    May 31st

--- ReadInData.sh
+++ ReadInData.sh
@@ -2,8 +2,8 @@
 
 export GISRC=/home/1te/.grassrc6.data
 
-GEE_FILES_IN=1
-FW3_REPROJECT=0
+GEE_FILES_IN=0
+FW3_REPROJECT=1
 
 #############################################################                                                       
 if [ $GEE_FILES_IN -eq 1 ]
@@ -27,20 +27,26 @@ fi
 #############################################################                                                       
 if [ $FW3_REPROJECT -eq 1 ]
 then
- input=2023.trueS3.S3?-NT.napolygon.maxndvi.LAEA.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc -g
- g.region n=48:20:42.302206N s=22:13:42.383255N w=119:32:21.484587W e=62:33:16.177794W rows=14276 cols=20170 -p
+ r.proj input=2022.trueS3.S3?-NT.napolygon.maxndvi.LAEA.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc -g
+# g.region n=44:08:08.547362N s=8:44:35.871663N w=117:41:31.873241W e=42:03:01.170114W rows=22845 cols=26598
+ #n=48:20:42.302206N s=22:13:42.383255N w=119:32:21.484587W e=62:33:16.177794W rows=14276 cols=20170 -p
+ g.region rast=terraAquaCombined_1yr_auc_2020 -p
 
- r.proj input=2023.trueS3.S3?-NT.napolygon.maxndvi.LAEA.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc
- r.proj input=2022.trueS3.S3?-NT.napolygon.maxndvi.LAEA.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc
- r.proj input=2021.trueS3.S3?-NT.napolygon.maxndvi.LAEA.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc
- r.proj input=2020.trueS3.S3?-NT.napolygon.maxndvi.LAEA.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc
+ r.proj input=2023.trueS3.S3?-NT.napolygon.maxndvi.LAEA.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc --o
+d.rast 2023.trueS3.S3?-NT.napolygon.maxndvi.LAEA.rseriessum.45
+ r.proj input=2022.trueS3.S3?-NT.napolygon.maxndvi.LAEA.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc --o
+ r.proj input=2021.trueS3.S3?-NT.napolygon.maxndvi.LAEA.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc --o
+d.rast 2021.trueS3.S3?-NT.napolygon.maxndvi.LAEA.rseriessum.45
+ r.proj input=2020.trueS3.S3?-NT.napolygon.maxndvi.LAEA.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc --o
  
- r.proj input=fakeS3laea.maxMODIS.2019.std.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc
- r.proj input=fakeS3laea.maxMODIS.2018.std.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc
- r.proj input=fakeS3laea.maxMODIS.2017.std.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc
- r.proj input=fakeS3laea.maxMODIS.2016.std.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc
- r.proj input=fakeS3laea.maxMODIS.2015.std.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc
- r.proj input=fakeS3laea.maxMODIS.2014.std.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc
+ r.proj input=fakeS3laea.maxMODIS.2019.std.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc --o
+ r.proj input=fakeS3laea.maxMODIS.2018.std.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc --o
+ r.proj input=fakeS3laea.maxMODIS.2017.std.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc --o
+ r.proj input=fakeS3laea.maxMODIS.2016.std.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc --o
+ r.proj input=fakeS3laea.maxMODIS.2015.std.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc --o
+ r.proj input=fakeS3laea.maxMODIS.2014.std.rseriessum.45 location=CONUS_Phenology_LAEA mapset=FW_auc --o
 
+ r.proj input=phenology_2000-2016.500.maxmode location=CONUS_Phenology_LAEA mapset=phenoregions --o
 
 fi
+
